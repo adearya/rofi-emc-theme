594dc317d9f41003be0817980c23aaf8c389e53f
adearya <ade.aryabimantara@gmail.com>
2024-11-09 07:27:22 +0700

    Update everything

--- scripts/screenrecord.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-options="Monitor built-in audio input\nBuilt-in audio input"
-typeSource="$(echo -e $options | rofi -dmenu -theme $HOME/SourceCode/rofi-emc-theme/themes/screenrecord.rasi)"
-if [[ -n $screenshot ]]; then
-  sh -c "echo $typeSource > $HOME/.cache/screenrecord/typeSource"
-fi
